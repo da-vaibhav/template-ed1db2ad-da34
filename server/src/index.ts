4e54b5e75e9f127a296a69d4b7809428232efe55
vaibhav <da-vaibhav@users.noreply.github.com>
2025-03-11 07:44:50 +0530

    hono basics

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,37 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { env } from 'node:process';
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { compress } from 'hono/compress';
+import { secureHeaders } from 'hono/secure-headers';
 
-const app = new Hono()
+const app = new Hono();
+const PORT = Number(env.PORT) || 8787;
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(logger());
+app.use(compress());
+app.use(secureHeaders());
+
+app.get('/api/test', (c) => {
+  return c.json({
+    message: 'Hello from Hono!',
+    timestamp: new Date().toISOString(),
+  })
+});
+
+app.notFound((c) => {
+  return c.text('Custom 404 Message', 404)
+});
+
+app.onError((err, c) => {
+  console.error(`${err}`)
+  return c.text('An error occurred', 500)
+});
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: PORT,
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
-})
+});
+
